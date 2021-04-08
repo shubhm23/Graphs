ddcf449a860ed731e58a834239eee5ccaf683246
Shubham Prasad <74151301+shubhm23@users.noreply.github.com>
2021-04-09 00:33:05 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -57,13 +57,12 @@ int main(){
 
 	Graph g(10);
 	g.addEdge(0, 1);
-    g.addEdge(0, 4);
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
-    //printGraph(adj, V);
-    g.BFS(1);
+    	g.addEdge(0, 4);
+    	g.addEdge(1, 2);
+    	g.addEdge(1, 3);
+    	g.addEdge(1, 4);
+    	g.addEdge(2, 3);
+    	g.addEdge(3, 4);
+       	g.BFS(1);
 }
 
