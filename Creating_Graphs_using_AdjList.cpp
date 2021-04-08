2a7b946fcc49d13d4d35e0df3f098317e1a4ed46
Shubham Prasad <74151301+shubhm23@users.noreply.github.com>
2021-04-08 16:48:57 +0530

    Update Creating_Graphs_using_AdjList.cpp

--- Creating_Graphs_using_AdjList.cpp
+++ Creating_Graphs_using_AdjList.cpp
@@ -27,12 +27,12 @@ int main(){
 	int V = 5;
 	std::vector<int> adj[V];
 	addEdge(adj, 0, 1);
-    addEdge(adj, 0, 4);
-    addEdge(adj, 1, 2);
-    addEdge(adj, 1, 3);
-    addEdge(adj, 1, 4);
-    addEdge(adj, 2, 3);
-    addEdge(adj, 3, 4);
-    printGraph(adj, V);
+    	addEdge(adj, 0, 4);
+   	addEdge(adj, 1, 2);
+    	addEdge(adj, 1, 3);
+    	addEdge(adj, 1, 4);
+    	addEdge(adj, 2, 3);
+    	addEdge(adj, 3, 4);
+    	printGraph(adj, V);
 }
 
